9eb1dcae2b404959b5e0b7b72b94ea1e060d64af
ppashchenkov <email@example.com>
2025-12-08 12:26:01 +0300

    add files for Actions

--- tests/freestyle_project/freestyle_data.py
+++ tests/freestyle_project/freestyle_data.py
@@ -14,3 +14,16 @@ class Freestyle:
         "Terminate a build if it's stuck",
         'With Ant'
     ]
+    tooltip_environment = [
+        ('a[tooltip="Help for feature: Patterns for files to be deleted"]', 'tippy-53', 'Help for feature: Patterns for files to be deleted'),
+        ('a[tooltip="Help for feature: Apply pattern also on directories"]', 'tippy-54', 'Help for feature: Apply pattern also on directories'),
+        ('a[tooltip="Help for feature: Check parameter"]', 'tippy-55', 'Help for feature: Check parameter'),
+        ('a[tooltip="Help for feature: External Deletion Command"]', 'tippy-56', 'Help for feature: External Deletion Command'),
+        ('a[tooltip="Help for feature: Disable deferred wipeout"]', 'tippy-57', 'Help for feature: Disable deferred wipeout'),
+        ('a[tooltip="Help for feature: Use secret text(s) or file(s)"]', 'tippy-58', 'Help for feature: Use secret text(s) or file(s)'),
+        ('a[tooltip="Help for feature: Time-out strategy"]', 'tippy-59', 'Help for feature: Time-out strategy'),
+        ('a[tooltip="Help for feature: Absolute"]', 'tippy-62', 'Help for feature: Absolute'),
+        ('a[tooltip="Help for feature: Timeout minutes"]', 'tippy-63', 'Help for feature: Timeout minutes'),
+        ('a[tooltip="Help for feature: Time-out actions"]', 'tippy-60', 'Help for feature: Time-out actions'),
+        ('a[tooltip="Help for feature: With Ant"]', 'tippy-61', 'Help for feature: With Ant')
+    ]
--- tests/freestyle_project/test_environment_freestyle_project.py
+++ tests/freestyle_project/test_environment_freestyle_project.py
@@ -1,3 +1,4 @@
+import pytest
 from playwright.sync_api import expect
 
 from tests.freestyle_project.freestyle_data import Freestyle
@@ -28,3 +29,16 @@ def test_checkboxes_can_be_selected(freestyle):
         checkboxes.nth(i).locator(checkbox_loc).click()
 
         expect(checkboxes.nth(i).locator(checkbox_check_loc)).not_to_be_checked()
+
+@pytest.mark.parametrize('tp_link, tippy, tp_expected_text', Freestyle.tooltip_environment)
+def test_environment_tooltips(freestyle, tp_link, tippy, tp_expected_text) :
+    checkboxes = freestyle.locator('#environment ~ .jenkins-form-item')
+    checkbox_loc = '.row-set-start>.jenkins-checkbox-help-wrapper>.jenkins-checkbox'
+
+    for i in (0,4) :
+        checkboxes.nth(i).locator(checkbox_loc).click()
+    checkboxes.nth(0).get_by_text("Advanced").click()
+    freestyle.locator(tp_link).hover()
+    tp_text = freestyle.locator(f'div#{tippy}')
+
+    expect(tp_text).to_have_text(tp_expected_text)
