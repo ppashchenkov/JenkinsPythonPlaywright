16666d50118d73a4470bc4825e71fc6438e3e01f
ppashchenkov <email@example.com>
2025-12-08 13:34:50 +0300

    refactor conftest.py

--- conftest.py
+++ conftest.py
@@ -21,7 +21,7 @@ def get_cookie(playwright: Playwright):
         base_url=BASE_URL
     )
     page = context.new_page()
-    # page.wait_for_timeout(300)
+
     name_loc = "input[id='j_username']"
     password_loc = "input[id='j_password']"
     submit_btn_loc = "button[name='Submit']"
